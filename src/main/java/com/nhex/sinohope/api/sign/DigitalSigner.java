63c60b16147f1d8e3fa10c2c8cab3ab178e70356
lishuo <lishuo@apache.org>
2023-05-24 14:39:25 +0800

    [feature: impl ecdsa algorithm]

---
+++ src/main/java/com/nhex/sinohope/api/sign/DigitalSigner.java
@@ -0,0 +1,104 @@
+package com.nhex.sinohope.api.sign;
+
+import java.security.Key;
+import java.security.KeyFactory;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.NoSuchAlgorithmException;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.security.SecureRandom;
+import java.security.Signature;
+import java.security.interfaces.ECPrivateKey;
+import java.security.interfaces.ECPublicKey;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.X509EncodedKeySpec;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * DigitalSigner
+ * @author lishuo
+ * @date 2023/5/24 10:46
+ **/
+public class DigitalSigner {
+    private static final String PUBLIC_KEY = "";
+    private static final String PRIVATE_KEY = "";
+
+    /**
+     * @return Map pub、pri key
+     */
+    public static Map<String, Object> initKey() {
+        try {
+            KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("EC");
+            keyPairGenerator.initialize(256, new SecureRandom());
+            KeyPair keyPair = keyPairGenerator.generateKeyPair();
+            ECPublicKey publicKey = (ECPublicKey) keyPair.getPublic();
+            ECPrivateKey privateKey = (ECPrivateKey) keyPair.getPrivate();
+            Map<String, Object> keyMap = new HashMap<>(2);
+            keyMap.put(PUBLIC_KEY, publicKey);
+            keyMap.put(PRIVATE_KEY, privateKey);
+            return keyMap;
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        return Collections.emptyMap();
+    }
+
+    /**
+     * @param data signData
+     * @param privKey  privatekey
+     * @return byte[] crypt data
+     */
+    public static byte[] digitalSign(byte[] data, byte[] privKey) {
+        try {
+            PrivateKey privateKey = (PrivateKey) KeyFactory.getInstance("EC")
+                    .generatePrivate(new PKCS8EncodedKeySpec(privKey));
+            Signature signature = Signature.getInstance("SHA256WithECDSA");
+            signature.initSign(privateKey);
+            signature.update(data);
+            return signature.sign();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        return null;
+    }
+
+    /**
+     * @param data
+     * @param pubKey  publicKey
+     * @return verify result
+     */
+    public static boolean signVerify(byte[] data, byte[] rsaData, byte[] pubKey) {
+        try {
+            PublicKey publicKey = (PublicKey) KeyFactory.getInstance("EC")
+                    .generatePublic(new X509EncodedKeySpec(pubKey));
+            Signature signature = Signature.getInstance("SHA256WithECDSA");
+            signature.initVerify(publicKey);
+            signature.update(data);
+            return signature.verify(rsaData);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        return false;
+    }
+
+    /**
+     * @param keyMap
+     * @return byte[] privateKey
+     */
+    public static byte[] getPrivateKey(Map<String, Object> keyMap) {
+        Key key = (Key) keyMap.get(PRIVATE_KEY);
+        return key.getEncoded();
+    }
+
+    /**
+     * @param keyMap
+     * @return byte[] publicKey
+     */
+    public static byte[] getPublicKey(Map<String, Object> keyMap) {
+        Key key = (Key) keyMap.get(PUBLIC_KEY);
+        return key.getEncoded();
+    }
+}
---
+++ src/test/java/com/nhex/sinohope/api/sign/DigitalSignerTest.java
@@ -0,0 +1,64 @@
+package com.nhex.sinohope.api.sign;
+
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import java.util.Map;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+/**
+ * com.nhex.sinohope.api.sign.DigitalSignerTest
+ *
+ * @author lishuo
+ * @date 2023/5/24 10:46
+ **/
+public class DigitalSignerTest {
+
+    private static byte[] publicKey;
+    private static byte[] privateKey;
+
+    private static String data = "{\n" +
+            "  \"name\": \"John Doe\",\n" +
+            "  \"age\": 30,\n" +
+            "  \"email\": \"johndoe@example.com\",\n" +
+            "  \"address\": {\n" +
+            "    \"street\": \"123 Main St\",\n" +
+            "    \"city\": \"New York\",\n" +
+            "    \"state\": \"NY\",\n" +
+            "    \"postalCode\": \"10001\"\n" +
+            "  },\n" +
+            "  \"phoneNumbers\": [\n" +
+            "    \"123-456-7890\",\n" +
+            "    \"987-654-3210\"\n" +
+            "  ],\n" +
+            "  \"isEmployed\": true,\n" +
+            "  \"education\": {\n" +
+            "    \"degree\": \"Bachelor's\",\n" +
+            "    \"major\": \"Computer Science\"\n" +
+            "  },\n" +
+            "  \"interests\": [\"reading\", \"traveling\", \"cooking\"],\n" +
+            "  \"lastLogin\": \"2023-05-18T12:00:00Z\"\n" +
+            "}\n";
+
+    @BeforeAll
+    public static void setupKeys() {
+        Map<String, Object> keyMap = DigitalSigner.initKey();
+        publicKey = DigitalSigner.getPublicKey(keyMap);
+        privateKey = DigitalSigner.getPrivateKey(keyMap);
+    }
+
+    @Test
+    public void testKeyGeneration() {
+        assertTrue(publicKey != null && publicKey.length > 0);
+        assertTrue(privateKey != null && privateKey.length > 0);
+    }
+
+    @Test
+    public void testSignAndVerify() {
+        byte[] signature = DigitalSigner.digitalSign(data.getBytes(), privateKey);
+        assertTrue(signature != null && signature.length > 0);
+
+        boolean verified = DigitalSigner.signVerify(data.getBytes(), signature, publicKey);
+        assertTrue(verified);
+    }
+}
